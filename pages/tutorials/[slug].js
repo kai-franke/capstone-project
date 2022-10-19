ea24b15433c1160ba45ce474fe28aae9c4a93742
Kai Franke <cosinushh@gmail.com>
2022-10-19 14:16:19 +0200

    change dynamic routing from <id> to <slug>

---
+++ pages/tutorials/[slug].js
@@ -0,0 +1,39 @@
+import {
+  getAllTutorials,
+  getTutorialBySlug,
+} from "../../services/tutorialService";
+
+export async function getStaticPaths() {
+  const tutorials = await getAllTutorials();
+  const slugs = tutorials.map((tutorial) => tutorial.slug);
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { slug } = context.params;
+  const tutorial = await getTutorialBySlug(slug);
+
+  return {
+    props: {
+      name: tutorial.name,
+      steps: tutorial.steps,
+      id: tutorial.id,
+      slug: slug,
+    },
+  };
+}
+
+export default function Tutorial({ name, steps, id, slug }) {
+  console.log(steps);
+  return (
+    <>
+      <h1>{name}</h1>
+      <p>id: {id}</p>
+      <p>slug: {slug}</p>
+    </>
+  );
+}
--- services/tutorialService.js
+++ services/tutorialService.js
@@ -1,10 +1,14 @@
 const tutorials = [
   {
-    id: "b74d7e1c-acc4-4f65-bfd9-aeaa70dd6878",
+    id: "5e0d66cb-4b7f-4eec-b3c0-3591603cd233",
     name: "Bathing a hamster",
     cover: "/assets/tutorial-1_cover-image_1900x1900.jpg",
-    path: "bathing-a-hamster",
+    slug: "bathing-a-hamster",
     steps: [
+      {
+        step: 0,
+        title: "Start",
+      },
       {
         step: 1,
         title: "Steal a hamster",
@@ -33,13 +37,17 @@ const tutorials = [
           "Step 4 description. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ",
         img: "/assets/tutorial-1_cover-image_1900x1900.jpg",
       },
+      {
+        step: 5,
+        title: "End",
+      },
     ],
   },
   {
-    id: "6c3158b7-908a-4472-8379-34296a31009d",
+    id: "1b9d100e-442d-47a5-9a35-2c302466de1e",
     name: "Baking sourdough bread",
     cover: "/assets/tutorial-2_cover-image_2000x2000.jpg",
-    path: "baking-sourdough-bread",
+    slug: "baking-sourdough-bread",
     steps: [
       {
         step: 1,
@@ -72,10 +80,10 @@ const tutorials = [
     ],
   },
   {
-    id: "01a29dba-6feb-401c-85eb-77f58d5761d8",
+    id: "653131b5-52fa-4b73-909b-419a6f87697d",
     name: "Building a helpful robot",
     cover: "/assets/tutorial-3_cover-image_2000x2000.jpg",
-    path: "building-a-helpful-robot",
+    slug: "building-a-helpful-robot",
     steps: [
       {
         step: 1,
@@ -105,8 +113,12 @@ const tutorials = [
 export async function getAllTutorials() {
   return tutorials;
 }
+
 /* 
 export async function getTutorialById(id) {
   return tutorials.find((tutorial) => tutorial.id === id);
+} */
+
+export async function getTutorialBySlug(slug) {
+  return tutorials.find((tutorial) => tutorial.slug === slug);
 }
- */
