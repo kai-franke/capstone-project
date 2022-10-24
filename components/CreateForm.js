c7d68f50a946e3f7cc42044c7d24f6792fbda295
Kai Franke <cosinushh@gmail.com>
2022-10-24 18:02:06 +0200

    add auto-scroll effect

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -1,17 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import styled from "styled-components";
 import { Button, ButtonContainer } from "./Buttons";
 
 function CreateForm() {
   const [steps, setSteps] = useState(1);
+  const buttonRef = useRef();
+
+  function scrollToButton() {
+    setTimeout(() => {
+      buttonRef.current.scrollIntoView({ behavior: "smooth" });
+    });
+  }
 
   function handleAddStep() {
     setSteps((prevSteps) => prevSteps + 1);
+    scrollToButton();
   }
 
   return (
     <>
-      <FormContainer id="createForm">
+      <FormContainer>
         <FormCard>
           <StyledLabel isPrimary>
             <LabelText>Tutorial title</LabelText>
@@ -23,37 +31,35 @@ function CreateForm() {
         </FormCard>
         {[...Array(steps)].map((step, index) => {
           return (
-            <>
-              <FormCard key={index}>
-                <StepNumber>Step {index + 1}</StepNumber>
-                <StyledLabel isPrimary={false}>
-                  <LabelText>Step title</LabelText>
-                  <StyledInput
-                    placeholder="e.g. Prepare your tools"
-                    aria-placeholder="e.g. Prepare your tools"
-                  />
-                </StyledLabel>
-                <StyledLabel isPrimary={false}>
-                  <LabelText>Picture URL</LabelText>
-                  <StyledInput
-                    placeholder="https://www..."
-                    aria-placeholder="https://www..."
-                  />
-                </StyledLabel>
-                <StyledLabel isPrimary={false}>
-                  <LabelText>Step description</LabelText>
-                  <StyledTextarea
-                    placeholder="Enter a description"
-                    aria-placeholder="Enter a description"
-                  />
-                </StyledLabel>
-              </FormCard>
-            </>
+            <FormCard key={index.toString()}>
+              <StepNumber>Step {index + 1}</StepNumber>
+              <StyledLabel isPrimary={false}>
+                <LabelText>Step title</LabelText>
+                <StyledInput
+                  placeholder="e.g. Prepare your tools"
+                  aria-placeholder="e.g. Prepare your tools"
+                />
+              </StyledLabel>
+              <StyledLabel isPrimary={false}>
+                <LabelText>Picture URL</LabelText>
+                <StyledInput
+                  placeholder="https://www..."
+                  aria-placeholder="https://www..."
+                />
+              </StyledLabel>
+              <StyledLabel isPrimary={false}>
+                <LabelText>Step description</LabelText>
+                <StyledTextarea
+                  placeholder="Enter a description"
+                  aria-placeholder="Enter a description"
+                />
+              </StyledLabel>
+            </FormCard>
           );
         })}
       </FormContainer>
       <ButtonContainer>
-        <Button isPrimary onClick={handleAddStep}>
+        <Button ref={buttonRef} isPrimary onClick={handleAddStep}>
           Add step
         </Button>
       </ButtonContainer>
@@ -75,8 +81,6 @@ const FormCard = styled.fieldset`
   background-color: var(--white);
   padding: 0.7em;
   display: grid;
-  //column-gap: 0.7em;
-  //grid-template-columns: 5.625em 1fr;
   box-shadow: 0px 12px 16px -4px rgba(16, 24, 40, 0.08),
     0px 4px 6px -2px rgba(16, 24, 40, 0.03);
   transition: 300ms linear;
@@ -109,6 +113,7 @@ const StyledTextarea = styled.textarea`
   border: 1px solid var(--gray-30);
   border-radius: 8px;
   padding: 0.4em;
+  height: 4.5em;
 
   &::placeholder {
     color: var(--gray-30);
