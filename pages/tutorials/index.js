3fb1c48c5eb7832c6163a9cf72c2750e615cd8bc
Kai Franke <cosinushh@gmail.com>
2022-10-30 18:53:47 +0100

    change to client side tutorial fetching in order to show newly added tutorials without reload of library page

--- pages/tutorials/index.js
+++ pages/tutorials/index.js
@@ -1,16 +1,33 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Headline from "../../components/Headline";
 import TutorialList from "../../components/TutorialList";
-import { getAllTutorials } from "../../services/tutorialService";
+//import { getAllTutorials } from "../../services/tutorialService";
 
-export async function getServerSideProps(context) {
+/* export async function getServerSideProps(context) {
   const tutorials = await getAllTutorials();
   return {
     props: { tutorials: tutorials },
   };
-}
+} */
 
-export default function TutorialsPage({ tutorials }) {
+export default function TutorialsPage() {
+  const [tutorials, setTutorials] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/tutorials")
+      .then((res) => res.json())
+      .then((tutorials) => {
+        setTutorials(tutorials);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <Message>Loading...</Message>;
+  if (!tutorials) return <Message>No tutorial data</Message>;
+
   return (
     <>
       <Headline>My tutorials</Headline>
