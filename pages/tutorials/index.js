d63c1b191a2c3b6b2c26a37c2adbc2289e758675
Kai Franke <cosinushh@gmail.com>
2022-10-30 19:29:30 +0100

    refactor loading message

--- pages/tutorials/index.js
+++ pages/tutorials/index.js
@@ -25,23 +25,24 @@ export default function TutorialsPage() {
       });
   }, []);
 
-  /* if (isLoading) return <Message>Loading...</Message>;
-  if (!tutorials) return <Message>No tutorial data</Message>;
- */
+  if (isLoading)
+    return (
+      <>
+        <Headline>My tutorials</Headline>
+        <Message>Loading...</Message>
+      </>
+    );
+  if (!tutorials)
+    return (
+      <>
+        <Headline>My tutorials</Headline>
+        <Message>No tutorial data</Message>
+      </>
+    );
+
   return (
     <>
       <Headline>My tutorials</Headline>
-      {isLoading && (
-        <>
-          <Message>Loading...</Message>
-        </>
-      )}
-      ;
-      {!tutorials && (
-        <>
-          <Message>No tutorial data</Message>
-        </>
-      )}
       <TutorialList tutorials={tutorials} />
       <Message>No more tutorials</Message>
     </>
