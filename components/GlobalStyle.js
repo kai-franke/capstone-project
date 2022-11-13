bc1f428a8b89e031895f9586314d3da9fbf46d9b
Kai Franke <cosinushh@gmail.com>
2022-11-13 14:42:46 +0100

    optimize global box-shadow

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -18,8 +18,8 @@ const GlobalStyle = createGlobalStyle`
           --gray-30: #D0D5DD;
           --gray-10: #f5f5f5;
 
-          --boxshadow-primary: 0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03);
-        
+          --boxshadow-primary: hsl(0 0% 0% / 10%) 0px 1px 2px 0px;
+     
       }
   
       /* poppins-100 - latin */
